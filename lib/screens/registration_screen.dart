574b7b1fb05ce77d3a8e60c0953742f730708889
Yousef Amr <yoyo2007pc>
2025-11-29 01:31:25 +0200

    Last try to fix the issue

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -69,9 +69,7 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
                   final newUser =
                       await _auth.createUserWithEmailAndPassword(
                           email: email, password: password);
-                  if (newUser != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  }
+                  Navigator.pushNamed(context, ChatScreen.id);
                 } catch (e) {
                   print(e);
                 }
